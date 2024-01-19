59d999f759cae3b443d997d7fade580ea18cbd04
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-19 12:55:28 +0200

    Create solution for data_types task.

---
+++ 02_week/data_types.js
@@ -0,0 +1,31 @@
+/*
+- Create a variable named age and assign it an integer value.
+- Create a variable named name and assign it a string value.
+- Create a variable named isStudent and assign it a boolean value.
+- Create a variable named hobbies and assign it an array containing a few string values (e.g., hobbies).
+- Create an info object with two properties: schoolName (a string) and grade (an integer).
+
+- Use console.log() to print each variable to the console.
+
+- Use the typeof operator to check the data type of each variable and print the result using console.log(). */
+
+let age = 25;
+let name = "Matti Meikäläinen";
+let isStudent = true;
+let hobbies = ["Cycling", "Padel", "Video games", "Music"];
+const info = {
+    schoolName: "Business College Helsinki",
+    grade: 5
+};
+
+console.log(age);
+console.log(name);
+console.log(isStudent);
+console.log(hobbies);
+console.log(info);
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudent);
+console.log(typeof hobbies)
+console.log(typeof info);
