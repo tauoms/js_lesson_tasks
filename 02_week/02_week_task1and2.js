6a9ab4ed0e59f11d7be5904bb50f8b70ddda8341
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-19 10:22:41 +0200

    First commit

---
+++ 02_week/02_week_task1and2.js
@@ -0,0 +1,9 @@
+// Define a variable count with original value of 0 and then increment it
+// (add 1 to it) on the following line.
+
+let count = 0;
+++ count;
+console.log(count);
+
+const ageLimit = 18;
+console.log(ageLimit);
---
+++ 02_week/02_week_task3.js
@@ -0,0 +1,13 @@
+// Declare two variables: student and name.
+// Assign the value "Mikko" to name.
+// Copy the value from name to student.
+// Console.log the value of student (must ouput "Mikko").
+
+let student, name;
+name = "Mikko";
+student = name;
+console.log(student);
+
+// Why is name strikethrough?
+// name is an obsolete property of the Window object. 
+// When you see a strikethrough on a variable, VS Code knows that it is deprecated.
