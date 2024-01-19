6b3261b0ebbb05165764fc9475f0dba2d74766d5
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-19 14:08:37 +0200

    Create solution for basic_02 task.

---
+++ 02_week/basic_02.js
@@ -0,0 +1,9 @@
+// Write the function multiply such that it returns the product (the result of multiplication) of a and b.
+
+function multiply(x, y) {
+    return x * y;
+}
+
+// sample usage - do not modify
+console.log(multiply(2, 4));
+console.log(multiply(3, 2));
