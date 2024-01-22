df2da86d308dc7e275bdbef6948a665910a3a09b
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-22 13:01:08 +0200

    Add solution for strings_09 task.

---
+++ 02_week/strings/strings_09.js
@@ -0,0 +1,11 @@
+/**
+Refine the getFullName function so that it returns the person's full name by utilizing string interpolation.
+ */
+
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
+
+// Sample usage - do not modify
+console.log(getFullName('Sam', 'Doe')); // "Sam Doe"
+console.log(getFullName('Alex', 'Blue')); // "Alex Blue"
