9c61bb1da175aedd76f15e47eee01816ae3fdc92
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-22 14:45:52 +0200

    Add solution for conditions_06.

---
+++ 03_week/03_exerices/conditions/conditions_06.js
@@ -0,0 +1,18 @@
+/**
+Develop a function called greaterNum which:
+
+ - Accepts two arguments, both of which are numbers.
+ - Returns the larger of the two numbers.
+
+ */
+
+// Sample usage - do not modify
+
+function greaterNum (firstNum, secondNum) {
+    if (firstNum > secondNum) return firstNum;
+    else return secondNum;
+}
+
+console.log(greaterNum(5, 10));
+console.log(greaterNum(2, 1));
+console.log(greaterNum(4, 25));
