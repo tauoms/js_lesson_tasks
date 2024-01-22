35bdf2362f16727a4d7168be69fd561aa6ec78e4
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-22 12:48:32 +0200

    Add solution for strings_04 task.

---
+++ 02_week/strings/strings_04.js
@@ -0,0 +1,11 @@
+/**
+Refine the getFirstCharacter function to return the first character of the name parameter provided to it.
+ */
+
+function getFirstCharacter(name) {
+    return name[0];
+}
+
+// Sample usage - do not modify
+console.log(getFirstCharacter('Amsterdam')); // "A"
+console.log(getFirstCharacter('Japan')); // "J"
