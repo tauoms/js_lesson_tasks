e3a8f00d649063a35873a554126d0b383fb5e669
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-22 12:58:16 +0200

    Add solution for strings_07.

---
+++ 02_week/strings/strings_07.js
@@ -0,0 +1,11 @@
+/**
+Refine the concatInitials function to return a string that combines the firstNameInitial followed by the lastNameInitial.
+ */
+
+function concatInitials(firstNameInitial, lastNameInitial) {
+    return firstNameInitial + lastNameInitial;
+}
+
+// Sample usage - do not modify
+console.log(concatInitials('J', 'D')); // "JD"
+console.log(concatInitials('S', 'B')); // "SB"
