41fd68d93e8406ae844c4debb8f5992aa64f94fd
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-22 12:55:48 +0200

    Add solution for strings_06 task.

---
+++ 02_week/strings/strings_06.js
@@ -0,0 +1,11 @@
+/**
+Refine the skipFirstCharacter function to return a string containing all characters from the text parameter, excluding the first character.
+
+ */
+function skipFirstCharacter(text) {
+    return text.substring(1);
+}
+
+// Sample usage - do not modify
+console.log(skipFirstCharacter('Xcode')); // "code"
+console.log(skipFirstCharacter('Hello')); // "ello"
