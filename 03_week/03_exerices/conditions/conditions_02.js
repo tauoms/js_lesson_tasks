20219279e0de7846d6c305b16d059e8bc7121023
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-22 14:36:05 +0200

    Add solution for conditions_02 task.

---
+++ 03_week/03_exerices/conditions/conditions_02.js
@@ -0,0 +1,9 @@
+/*
+Create a program getTemperature that asks the user to input a temperature in Celsius and then outputs whether they should wear a jacket (if the temperature is below 15°C) or not.
+ */
+function getTemperature (tempCelsius) {
+    if (tempCelsius < 15) return 'The temperature is below 15°C, you should wear a jacket.'
+    else return 'The temperature is above 15°C, no need for a jacket!'
+}
+
+getTemperature(Number(prompt('What is the current temperature?')));
