1417747cee86ee251a25020a955aa683f5813d1c
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-26 12:36:38 +0200

    Add console logs to returns.

--- 03_week/03_exerices/conditions/conditions_02.js
+++ 03_week/03_exerices/conditions/conditions_02.js
@@ -2,8 +2,9 @@
 Create a program getTemperature that asks the user to input a temperature in Celsius and then outputs whether they should wear a jacket (if the temperature is below 15°C) or not.
  */
 function getTemperature (tempCelsius) {
-    if (tempCelsius < 15) return 'The temperature is below 15°C, you should wear a jacket.'
-    else return 'The temperature is above 15°C, no need for a jacket!'
+    if (tempCelsius < 15) 
+    return console.log('The temperature is below 15°C, you should wear a jacket.')
+    else return console.log('The temperature is above 15°C, no need for a jacket!')
 }
 
 getTemperature(Number(prompt('What is the current temperature?')));
