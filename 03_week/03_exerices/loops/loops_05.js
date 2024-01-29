7b411e7ade7a88154600cb91bb7e6eb219194d9c
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-29 14:17:14 +0200

    Move inputNumber inside while loop.

--- 03_week/03_exerices/loops/loops_05.js
+++ 03_week/03_exerices/loops/loops_05.js
@@ -1,11 +1,10 @@
 // Develop a program that continuously prompts the user to input numbers until the user enters 0, at which point the program terminates. After termination, the program should calculate and display the average of all the entered numbers.
 
-let inputNumber;
 let amountOfNumbers = 0;
 let sumOfNumbers = 0;
 
 while (inputNumber !== 0) {
-    inputNumber = Number(prompt('Please input a number (Enter 0 to stop):'));
+    let inputNumber = Number(prompt('Please input a number (Enter 0 to stop):'));
     if (inputNumber == 0) break;
     amountOfNumbers++;
     sumOfNumbers += inputNumber;
