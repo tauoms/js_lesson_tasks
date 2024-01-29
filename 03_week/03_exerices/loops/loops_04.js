17817987a0bc4411ace3e98f6e302e67517377c5
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-01-29 14:14:05 +0200

    Move currentNumber variable inside for loop

--- 03_week/03_exerices/loops/loops_04.js
+++ 03_week/03_exerices/loops/loops_04.js
@@ -1,10 +1,9 @@
 // Develop a program that requests the user to input 20 numbers. After all numbers are entered, the program should display how many of these numbers are even.
 
 let evenNumbersCounter = 0;
-let currentNumber = 0;
 
 for (let i = 1; i <= 20; i++) {
-    currentNumber = Number(prompt(`Please enter any number. (${i}/20)`));
+    let currentNumber = Number(prompt(`Please enter any number. (${i}/20)`));
     if (currentNumber % 2 === 0) evenNumbersCounter++;
 }
 
