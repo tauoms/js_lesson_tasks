32f9b4fa817adb8202c448ad85ca7465c1cefffd
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-02-02 11:05:26 +0200

    Add basic solution for task basic DOM 1.

---
+++ 04_week/04_exerices/basic_DOM/basic_DOM_1/script.js
@@ -0,0 +1,11 @@
+/* 
+
+Write JavaScript code that selects content id by using getElementById.
+Write a function which will update the text content of the selected element to "Hello, World!" by clicking the button.
+*/
+
+const textHeading = document.getElementById('content');
+
+function changeHeading () {
+    textHeading.textContent = 'Hello, World!';
+}
