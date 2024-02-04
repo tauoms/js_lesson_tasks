3792f3fcd83de007fabad1a08afb932384d41439
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-02-04 12:27:30 +0200

    Add buttons and functions for Add Pancake + Hide/Show Pancakes. Add style.

--- 04_week/04_exerices/basic_DOM/basic_DOM_2/script.js
+++ 04_week/04_exerices/basic_DOM/basic_DOM_2/script.js
@@ -11,9 +11,15 @@ Ensure the input field is cleared after adding the fruit to the list.
 
 */
 
-const button = document.getElementById('addFruitBtn');
+const addFruitBtn = document.getElementById('addFruitBtn');
+const addPancakeBtn = document.getElementById('addPancakeBtn');
+const hideShowPancakesBtn = document.getElementById('hideShowPancakesBtn');
 
-button.addEventListener('click', addFruit);
+let pancakesVisible = true;
+
+addFruitBtn.addEventListener('click', addFruit);
+addPancakeBtn.addEventListener('click', addPancake);
+hideShowPancakesBtn.addEventListener('click', hideShowPancakes);
 
 function addFruit () {
     const fruitInput = document.getElementById('fruitInput');
@@ -22,4 +28,31 @@ function addFruit () {
     addLi.appendChild(document.createTextNode(fruitInput.value));
     fruitList.appendChild(addLi);
     fruitInput.value = '';
+}
+
+function addPancake () {
+    const fruitList = document.getElementById('fruitList');
+    const addLi = document.createElement('li');
+    addLi.classList.add('pancakeLi');
+    addLi.appendChild(document.createTextNode('I AM A PANCAKE.'));
+    fruitList.appendChild(addLi);
+}
+
+function hideShowPancakes () {
+    const pancakes = document.querySelectorAll('.pancakeLi')
+    if (pancakesVisible) {
+        hideShowPancakesBtn.textContent = 'Show Pancakes';
+        pancakes.forEach(element => { element.style.display = 'none';
+            
+        });
+        pancakesVisible = false;
+
+    } else {
+        hideShowPancakesBtn.textContent = 'Hide Pancakes';
+        pancakes.forEach(element => { element.style.display = 'list-item';
+            
+        });
+        pancakesVisible = true;
+    }
+
 }
