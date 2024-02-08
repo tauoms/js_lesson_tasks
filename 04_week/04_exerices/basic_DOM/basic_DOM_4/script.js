442a6858f45bcd3e2f3997861f8ff4fa934c23b6
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-02-08 20:00:58 +0200

    Shorten code by implementing querySelectorAll('input[type=checkbox]'.

--- 04_week/04_exerices/basic_DOM/basic_DOM_4/script.js
+++ 04_week/04_exerices/basic_DOM/basic_DOM_4/script.js
@@ -1,46 +1,23 @@
-// What I did initially (inside the function):
-    
-    // if(document.querySelector("#nuts").checked) {
-    //     priceCounter += Number(nuts.value);
-    // } if(document.querySelector("#bananas").checked) {
-    //     priceCounter += Number(bananas.value); 
-    // } if(document.querySelector("#syrup").checked) {
-    //     priceCounter += Number(syrup.value);
-    // } if(document.querySelector("#whippedCream").checked) {
-    //     priceCounter += Number(whippedCream.value);
-    // } if(document.querySelector("#iceCream").checked) {
-    //     priceCounter += Number(iceCream.value);
-    // } 
 
 
 const type = document.querySelector('#type');
-const nuts = document.querySelector('#nuts');
-const bananas = document.querySelector('#bananas');
-const syrup = document.querySelector('#syrup');
-const whippedCream = document.querySelector('#whippedCream');
-const iceCream = document.querySelector('#iceCream');
-const totalPrice = document.querySelectorAll('.totalPrice');
+const form = document.querySelector('.form-container');
 
-const toppings = [nuts, bananas, syrup, whippedCream, iceCream];
-
-type.addEventListener('change', updatePrice);
-nuts.addEventListener('change', updatePrice);
-bananas.addEventListener('change', updatePrice);
-syrup.addEventListener('change', updatePrice);
-whippedCream.addEventListener('change', updatePrice);
-iceCream.addEventListener('change', updatePrice);
+form.addEventListener('change', updatePrice);
 
 function updatePrice () {
+    const toppings = document.querySelectorAll('input[type="checkbox"]');
+    const totalPrice = document.querySelectorAll('.totalPrice');
+
     let priceCounter = Number(type.value);
 
-    // Loop through 'toppings' array (line 24)
     toppings.forEach((currentElement => {
         if (currentElement.checked) {
         priceCounter += Number(currentElement.value)
         }
     }));
 
-    // querySelectorAll (on line 22) returns an NodeList of matching elements. Loop through that list and assign total price stored in priceCounter.
+    // querySelectorAll (on line 10) returns an NodeList of matching elements. Loop through that list and assign total price stored in priceCounter.
     totalPrice.forEach(currentElement => currentElement.textContent = `${priceCounter}€`);
 
     // Trigger animation for price-banner span (thank you Stack Overflow).
@@ -76,7 +53,57 @@ const priceCalc = () => {
 };
 
 form.addEventListener('change', priceCalc);
-
-
-
 */
+
+// First working version:
+
+// const type = document.querySelector('#type');
+// const nuts = document.querySelector('#nuts');
+// const bananas = document.querySelector('#bananas');
+// const syrup = document.querySelector('#syrup');
+// const whippedCream = document.querySelector('#whippedCream');
+// const iceCream = document.querySelector('#iceCream');
+// const totalPrice = document.querySelectorAll('.totalPrice');
+
+// const toppings = [nuts, bananas, syrup, whippedCream, iceCream];
+
+// type.addEventListener('change', updatePrice);
+// nuts.addEventListener('change', updatePrice);
+// bananas.addEventListener('change', updatePrice);
+// syrup.addEventListener('change', updatePrice);
+// whippedCream.addEventListener('change', updatePrice);
+// iceCream.addEventListener('change', updatePrice);
+
+// function updatePrice () {
+//     let priceCounter = Number(type.value);
+
+//     // Loop through 'toppings' array (line 24)
+//     toppings.forEach((currentElement => {
+//         if (currentElement.checked) {
+//         priceCounter += Number(currentElement.value)
+//         }
+//     }));
+
+//     // querySelectorAll (on line 22) returns an NodeList of matching elements. Loop through that list and assign total price stored in priceCounter.
+//     totalPrice.forEach(currentElement => currentElement.textContent = `${priceCounter}€`);
+
+//     // Trigger animation for price-banner span (thank you Stack Overflow).
+//     const priceBannerSpan = document.querySelector('.price-banner > span');
+//     priceBannerSpan.style.animation = 'none';
+//     void priceBannerSpan.offsetWidth; // Trigger reflow
+//     priceBannerSpan.style.animation = 'zoomAndRotate 0.4s ease-in-out 1';
+// }
+
+// What I did initially (inside the function):
+    
+    // if(document.querySelector("#nuts").checked) {
+    //     priceCounter += Number(nuts.value);
+    // } if(document.querySelector("#bananas").checked) {
+    //     priceCounter += Number(bananas.value); 
+    // } if(document.querySelector("#syrup").checked) {
+    //     priceCounter += Number(syrup.value);
+    // } if(document.querySelector("#whippedCream").checked) {
+    //     priceCounter += Number(whippedCream.value);
+    // } if(document.querySelector("#iceCream").checked) {
+    //     priceCounter += Number(iceCream.value);
+    // } 
