62633fd46e0971d0e74ca6d1ddda63bec07e6ed2
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-02-12 14:57:01 +0200

    Add Place order button and order confirmation list.

---
+++ 04_week/pancake/script.js
@@ -0,0 +1,81 @@
+const form = document.querySelector('.form-container');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const typeSelect = document.querySelector('#type');
+const placeOrderBtn = document.querySelector('#placeorderbtn');
+
+let total;
+let toppings = [];
+let extras = [];
+
+const pancakePriceCalc = () => {
+  const totalPriceElement = document.querySelector('#totalPrice');
+  const priceBanner = document.querySelector('.price-banner');
+
+  total = parseInt(typeSelect.value);
+  totalPriceElement.textContent = `$${total}`;
+  
+  checkToppings();
+
+  priceBanner.animate(
+    [
+      { transform: 'scale(1)' },
+      { transform: 'scale(1.05)' },
+      { transform: ' scale(1)' },
+    ],
+    {
+      duration: 100,
+      iterations: 1,
+    }
+  );
+};
+
+const addItem = (itemName, category) => {
+  if (category === 'toppings') {
+    toppings.push(itemName);
+  } else {
+    extras.push(itemName);
+  }
+};
+
+const removeItem = (itemName, category) => {};
+
+const checkToppings = () => {
+  toppings = [];
+  extras = [];
+
+  for (const item of checkboxes) {
+    const itemName = item.dataset.name;
+    const category = item.dataset.category;
+
+    if (item.checked) {
+      total += parseInt(item.value);
+      addItem(itemName, category);
+    } else {
+      removeItem(itemName, category);
+    }
+  }
+
+  console.log('toppings array', toppings);
+  console.log('extras', extras);
+};
+
+const displayOrder = () => {
+  const nameField = document.querySelector('#namefield').value;
+  const orderType = document.querySelector('#order_type');
+  const orderToppings = document.querySelector('#order_toppings');
+  const orderExtras = document.querySelector('#order_extras');
+  const orderName = document.querySelector('#order_name');
+  const orderPrice = document.querySelector('#order_price');
+
+  orderType.textContent = typeSelect.selectedOptions[0].text.slice(0, -4);
+  orderToppings.textContent = toppings.join(', ');
+  orderExtras.textContent = extras.join(', ');
+  orderName.textContent = nameField;
+  orderPrice.textContent = `$${total}`;
+
+}
+
+
+
+form.addEventListener('change', pancakePriceCalc);
+placeOrderBtn.addEventListener('click', displayOrder);
