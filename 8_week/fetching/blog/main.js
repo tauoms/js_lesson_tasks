1e9afc009b2a196e85a15ad87fb93072bb4df9f2
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-08 19:59:40 +0200

    Add address info using de-structuring.

--- 8_week/fetching/blog/main.js
+++ 8_week/fetching/blog/main.js
@@ -17,6 +17,7 @@ const fetchData = async () => {
     }
 
     const json = await response.json();
+    console.log(json);
     displayData(json);
     } catch (error) {
         console.error(error);
@@ -29,27 +30,22 @@ fetchData();
 const displayData = (data) => {
     const postsContainer = document.querySelector('#postsContainer');
 
+
     data.forEach ((user) => {
+        const { street, suite, city, zipcode } = user.address;
+
         const postElement = document.createElement('div');
+
         postElement.innerHTML = `
         <h2>${user.name}</h2>
         <h3>ID: ${user.id}</h3>
-        <h3>username: ${user.username}</h3>
+        <h3><strong>username:</strong> ${user.username}</h3>
         <h3>${user.email}</h3>
+        <h3><strong>Address:</strong> ${street}, ${suite}, ${city}, ${zipcode}</h3>
         <h3>${user.phone}</h3>
         <h3>${user.website}</h3>
-        <h3>Company: ${user.company.name}</h3>
+        <h3><strong>Company:</strong> ${user.company.name}</h3>
         `;
         postsContainer.appendChild(postElement);
-
-        // const postHeading = document.createElement("h2");
-        // const titleNode = document.createTextNode(json.title);
-        // postHeading.appendChild(titleNode);
-        // postsContainer.appendChild(postHeading);
-
-        // const postBody = document.createElement("p");
-        // const bodyNode = document.createTextNode(json.body);
-        // postBody.appendChild(bodyNode);
-        // postsContainer.appendChild(postBody);
     });
 }
